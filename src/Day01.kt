f41c0457f0f0c2af75d0adb116dd0fa74c7757ac
tomoki1207 <tomoki1207@gmail.com>
2021-12-01 16:18:23 +0900

    Day1 solved

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.windowed(2)
+            .filter {
+                val prev = it[0].toInt()
+                val cur = it[1].toInt()
+                prev < cur
+            }.size
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.windowed(3)
+            .map { listOf(it[0].toInt(), it[1].toInt(), it[2].toInt()).sum().toString() }
+            .toList()
+            .let { part1(it) }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    println(part2(testInput))
 
     val input = readInput("Day01")
     println(part1(input))
