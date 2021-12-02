4eda53e446973c47c64da07ad67f2bc5d5d0184b
tomoki1207 <tomoki1207@gmail.com>
2021-12-02 18:09:28 +0900

    refactor: immutable data class

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,37 +1,37 @@
 fun main() {
-    data class Position(var horizontal: Int = 0, var depth: Int = 0, var aim: Int = 0)
+    data class Position(val horizontal: Int = 0, val depth: Int = 0, val aim: Int = 0) {
+        fun shift(horizontal: Int = 0, depth: Int = 0, aim: Int = 0): Position {
+            return Position(this.horizontal + horizontal, this.depth + depth, this.aim + aim)
+        }
+    }
 
     fun part1(input: List<String>): Int {
-        val pos = input.map { it.split(" ").let { part -> Pair(part[0], part[1]) } }
-            .fold(Position()) { acc, pair ->
-                val command = pair.first
-                val diff = pair.second.toInt()
+        return input.map { it.split(" ") }
+            .map { Pair(it[0], it[1].toInt()) }
+            .fold(Position()) { acc, (command, amount) ->
                 when (command) {
-                    "forward" -> acc.horizontal += diff
-                    "down" -> acc.depth += diff
-                    "up" -> acc.depth -= diff
+                    "forward" -> acc.shift(horizontal = amount)
+                    "down" -> acc.shift(depth = amount)
+                    "up" -> acc.shift(depth = -amount)
+                    else -> throw Exception()
                 }
-                acc
-            }
-        return pos.horizontal * pos.depth
+            }.let { it.horizontal * it.depth }
     }
 
     fun part2(input: List<String>): Int {
-        val pos = input.map { it.split(" ").let { part -> Pair(part[0], part[1]) } }
-            .fold(Position()) { acc, pair ->
-                val command = pair.first
-                val diff = pair.second.toInt()
+        return input.map { it.split(" ") }
+            .map { Pair(it[0], it[1].toInt()) }
+            .fold(Position()) { acc, (command, amount) ->
                 when (command) {
-                    "forward" -> {
-                        acc.horizontal += diff
-                        acc.depth += diff.times(acc.aim)
-                    }
-                    "down" -> acc.aim += diff
-                    "up" -> acc.aim -= diff
+                    "forward" -> acc.shift(
+                        horizontal = amount,
+                        depth = acc.aim * amount
+                    )
+                    "down" -> acc.shift(aim = amount)
+                    "up" -> acc.shift(aim = -amount)
+                    else -> throw Exception()
                 }
-                acc
-            }
-        return pos.horizontal * pos.depth
+            }.let { it.horizontal * it.depth }
     }
 
     val testInput = readInput("Day02_test")
