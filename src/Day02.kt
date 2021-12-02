a5393ab39c45e9d00e6aaac5f9dae8f2bb4bec5c
tomoki1207 <tomoki1207@gmail.com>
2021-12-02 17:03:25 +0900

    Day2 solved

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,11 +1,42 @@
 fun main() {
+    data class Position(var horizontal: Int = 0, var depth: Int = 0, var aim: Int = 0)
+
     fun part1(input: List<String>): Int {
-        return 0
+        val pos = input.map { it.split(" ").let { part -> Pair(part[0], part[1]) } }
+            .fold(Position()) { acc, pair ->
+                val command = pair.first
+                val diff = pair.second.toInt()
+                when (command) {
+                    "forward" -> acc.horizontal += diff
+                    "down" -> acc.depth += diff
+                    "up" -> acc.depth -= diff
+                }
+                acc
+            }
+        return pos.horizontal * pos.depth
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val pos = input.map { it.split(" ").let { part -> Pair(part[0], part[1]) } }
+            .fold(Position()) { acc, pair ->
+                val command = pair.first
+                val diff = pair.second.toInt()
+                when (command) {
+                    "forward" -> {
+                        acc.horizontal += diff
+                        acc.depth += diff.times(acc.aim)
+                    }
+                    "down" -> acc.aim += diff
+                    "up" -> acc.aim -= diff
+                }
+                acc
+            }
+        return pos.horizontal * pos.depth
     }
+
+    val testInput = readInput("Day02_test")
+    println(part1(testInput))
+    println(part2(testInput))
 
     val input = readInput("Day02")
     println(part1(input))
