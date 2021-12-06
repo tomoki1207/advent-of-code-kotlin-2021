4814178f2a4d06e3f615198c6d8fc6db5a59ab76
tomoki1207 <tomoki1207@gmail.com>
2021-12-06 17:25:49 +0900

    Day6 solved

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,10 +1,33 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+    fun countFishes(initial: List<Int>, days: Int): Long {
+        var timer = MutableList(9) { 0L }
+
+        // init
+        for (i in initial) {
+            timer[i]++
+        }
+
+        // apply days
+        for (i in 1..days) {
+            val reset = timer[0]
+            timer = timer.subList(1, 9)
+            // add reset lanternfish
+            timer[6] += reset
+            // new lanternfish
+            timer.add(reset)
+        }
+
+        return timer.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun prepare(input: List<String>) = input.first().split(",").map { it.toInt() }
+
+    fun part1(input: List<String>): Long {
+        return countFishes(prepare(input), 80)
+    }
+
+    fun part2(input: List<String>): Long {
+        return countFishes(prepare(input), 256)
     }
 
     val testInput = readInput("Day06_test")
