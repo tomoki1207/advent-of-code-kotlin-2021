9db29eaae0a0df04bb6ccf6a1bed4d15d889ad96
tomoki1207 <tomoki1207@gmail.com>
2021-12-12 03:49:00 +0900

    Day10 solved

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,10 +1,54 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+    fun discard(input: CharArray): Pair<Boolean, Long> {
+        val stack = ArrayDeque<Char>()
+        input.forEach { c ->
+            when (c) {
+                ')' -> {
+                    val last = stack.removeLast()
+                    if (last != '(') {
+                        return false to 3
+                    }
+                }
+                ']' -> {
+                    val last = stack.removeLast()
+                    if (last != '[') {
+                        return false to 57
+                    }
+                }
+                '}' -> {
+                    val last = stack.removeLast()
+                    if (last != '{') {
+                        return false to 1197
+                    }
+                }
+                '>' -> {
+                    val last = stack.removeLast()
+                    if (last != '<') {
+                        return false to 25137
+                    }
+                }
+                else -> stack.addLast(c)
+            }
+        }
+
+        // incomplete
+        return true to stack.reversed().fold(0L) { acc, c ->
+            acc * 5L + (mapOf('(' to 1, '[' to 2, '{' to 3, '<' to 4)[c]!!)
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): Long {
+        return input.map { discard(it.toCharArray()) }
+            .filter { !it.first }
+            .sumOf { it.second }
+    }
+
+    fun part2(input: List<String>): Long {
+        val incomplete = input.map { discard(it.toCharArray()) }
+            .filter { it.first }
+            .map { it.second }
+            .sortedBy { it }
+        return incomplete[incomplete.size / 2]
     }
 
     val testInput = readInput("Day10_test")
