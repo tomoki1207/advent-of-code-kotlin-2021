c7f0fc3816493e1a6e037e0dbe30e966ed8ec022
tomoki1207 <tomoki1207@gmail.com>
2021-12-13 17:08:17 +0900

    Day12 solved

--- src/Day12.kt
+++ src/Day12.kt
@@ -1,10 +1,44 @@
 fun main() {
+    data class Path(val p1: String, val p2: String) {
+        fun has(point: String) = p1 == point || p2 == point
+        fun another(point: String) = if (point == p1) p2 else p1
+    }
+
+    fun paths(input: List<String>) = input.map { line ->
+        val path = line.split("-")
+        Path(path.first(), path.last())
+    }
+
+    fun route(
+        point: String,
+        paths: List<Path>,
+        decideVisited: (point: String, visited: List<String>) -> Boolean,
+        visited: MutableList<String> = mutableListOf()
+    ): List<List<String>> {
+        visited.add(point)
+        if (point == "end") {
+            return listOf(visited)
+        }
+
+        val nextPoints = paths.filter { it.has(point) }
+            .map { it.another(point) }
+            .filterNot { decideVisited(it, visited) }
+
+        return nextPoints.flatMap { route(it, paths, decideVisited, visited.toMutableList()) }
+    }
+
     fun part1(input: List<String>): Int {
-        return 0
+        return route("start", paths(input), { p, visited ->
+            p == p.lowercase() && visited.contains(p)
+        }).size
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        return route("start", paths(input), { p, visited ->
+            val lowers = visited.filter { it == it.lowercase() }
+            p == "start" || (lowers.contains(p)
+                    && lowers.distinct().size != lowers.size)
+        }).size
     }
 
     val testInput = readInput("Day12_test")
