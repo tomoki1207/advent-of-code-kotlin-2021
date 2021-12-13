4745387f157974cecafcb85764b86e3cbabaa383
tomoki1207 <tomoki1207@gmail.com>
2021-12-13 09:57:09 +0900

    add Day12

---
+++ src/Day12.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    val testInput = readInput("Day12_test")
+    println(part1(testInput))
+    println(part2(testInput))
+
+    val input = readInput("Day12")
+    println(part1(input))
+    println(part2(input))
+}
