dbe50d44baeaf2fb965ca262ef8dd1de349723e5
tomoki1207 <tomoki1207@gmail.com>
2021-12-14 09:57:09 +0900

    Day13 solved

--- src/Day13.kt
+++ src/Day13.kt
@@ -1,10 +1,55 @@
 fun main() {
+    data class Point(val x: Int, val y: Int)
+
+    class Fold(command: String) {
+        val axis: String
+        val amount: Int
+
+        init {
+            val (axis, amount) = command.split(" ").last().split("=")
+            this.axis = axis
+            this.amount = amount.toInt()
+        }
+
+        fun fold(point: Point) = when (axis) {
+            "x" ->
+                if (point.x < amount) point else Point(point.x - ((point.x - amount) * 2), point.y)
+            "y" ->
+                if (point.y < amount) point else Point(point.x, point.y - ((point.y - amount) * 2))
+            else ->
+                throw Error()
+        }
+    }
+
+    fun prepare(input: List<String>): Pair<List<Point>, List<Fold>> {
+        val points = input.takeWhile { it.isNotBlank() }
+            .map { line ->
+                line.split(",")
+                    .let { Point(it.first().toInt(), it.last().toInt()) }
+            }
+        val commands = input.takeLastWhile { it.isNotBlank() }
+            .mapNotNull { if (it.startsWith("fold")) Fold(it) else null }
+        return Pair(points, commands)
+    }
+
     fun part1(input: List<String>): Int {
-        return 0
+        val (points, folds) = prepare(input)
+        val firstFold = folds.first()
+        return points.map { firstFold.fold(it) }.toSet().size
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val (points, folds) = prepare(input)
+        val result = folds.fold(points.toSet()) { acc, command ->
+            acc.map { command.fold(it) }.toSet()
+        }
+        for (y in 0..result.maxOf { it.y }) {
+            for (x in 0..result.maxOf { it.x }) {
+                result.contains(Point(x, y)).let { if (it) "#" else " " }.let { print(it) }
+            }
+            println()
+        }
+        return result.size
     }
 
     val testInput = readInput("Day13_test")
